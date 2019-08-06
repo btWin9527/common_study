6f7f1b3a8babd2af9184cccedaf9b46475d0336d
zhaoyagu <zhao.yaguo@shinewingdigital.com>
2019-08-06 14:07:24 +0800

    Object-api update

--- JavaScript/Object-API.js
+++ JavaScript/Object-API.js
@@ -8,3 +8,6 @@ console.log(Object.keys(obj))
 
 // Object.values 返回对象的value的一个集合（数组）
 console.log(Object.values(obj))
+
+// [ [ 'name', 'zhh' ], [ 'age', 12 ] ]  返回一个二维数组
+console.log(Object.entries(obj))
